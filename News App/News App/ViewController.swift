43464591b3f86fe899ecd3627a32674c77a77f55
vaishnavi <vasanjay@deloitte.com>
2024-06-12 12:47:57 +0530

    Basics

---
+++ News App/News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by Sanjay, Vaishnavi on 12/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
