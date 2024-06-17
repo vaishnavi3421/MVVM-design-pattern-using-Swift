a84c3d95a07bdb0e073d3f913ff5607ea178d625
vaishnavi <vasanjay@deloitte.com>
2024-06-17 22:11:55 +0530

    Basics

---
+++ News App/News App/Controller/NewsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  News Table View Controller.swift
+//  News App
+//
+//  Created by Sanjay, Vaishnavi on 13/06/24.
+//
+
+import Foundation
+ import UIKit
+
+class NewsTableViewController : UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
---
+++ News App/News App/Services/Web Services.swift
@@ -0,0 +1,8 @@
+//
+//  Web Services.swift
+//  News App
+//
+//  Created by Sanjay, Vaishnavi on 13/06/24.
+//
+
+import Foundation
